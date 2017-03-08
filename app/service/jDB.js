2b3adb7a813c15ecf4c44894cd88e6cd786465e2
shyclown <roman.moravcik1@gmail.com>
2017-03-08 23:50:13 +0100

    jDB

---
+++ app/service/jDB.js
@@ -0,0 +1,50 @@
+app.service('jDB',function(){
+  this.items = [
+    { id: 1, name: 'PHP', description: 'item description', price:15},
+    { id: 2, name: 'JAVASCRIPT', description: 'item description', price:15},
+    { id: 3, name: 'CSS', description: 'item description', price:8},
+    { id: 4, name: 'HTML', description: 'item description', price:8},
+    { id: 5, name: 'ANGULARJS', description: 'item description', price:12},
+    { id: 6, name: 'AUTOCAD', description: 'item description', price:20},
+    { id: 7, name: 'BLENDER', description: 'item description', price:15},
+    { id: 8, name: 'SKETCHUP', description: 'item description', price:15},
+    { id: 9, name: 'PHOTOSHOP', description: 'item description', price:15},
+    { id: 10, name: 'INDESIGN', description: 'item description', price:15},
+    { id: 11, name: 'SUBSTANCE PAINTER', description: 'item description', price:15},
+    { id: 12, name: 'SUBSTANCE DESIGNER', description: 'item description', price:15},
+    { id: 13, name: 'NODEJS', description: 'item description', price:15}
+
+  ];
+  this.categories = [
+    { id: 1, name: 'Backend', description: 'Description' },
+    { id: 2, name: 'Frontend', description: 'Description' },
+    { id: 3, name: 'Graphic design', description: 'Description' },
+    { id: 4, name: 'Architecture', description: 'Description' },
+    { id: 5, name: 'Web Design', description: 'Description' },
+    { id: 6, name: 'Game development', description: 'Description' },
+    { id: 7, name: '3D Modeling', description: 'Description' }
+  ];
+  this.item_category = [
+    { id: 1, category_id: 1, item_id: 1 },
+    { id: 2, category_id: 1, item_id: 13 },
+    { id: 3, category_id: 2, item_id: 5 },
+    { id: 4, category_id: 2, item_id: 4 },
+    { id: 5, category_id: 2, item_id: 3 },
+    { id: 6, category_id: 3, item_id: 9 },
+    { id: 7, category_id: 3, item_id: 10 },
+    { id: 8, category_id: 4, item_id: 6 },
+    { id: 9, category_id: 4, item_id: 8 },
+    { id: 10, category_id: 5, item_id: 4 },
+    { id: 11, category_id: 5, item_id: 3 },
+    { id: 12, category_id: 5, item_id: 9 },
+    { id: 13, category_id: 6, item_id: 7 },
+    { id: 14, category_id: 6, item_id: 11 },
+    { id: 15, category_id: 6, item_id: 12 },
+    { id: 16, category_id: 4, item_id: 7 },
+    { id: 17, category_id: 1, item_id: 2 },
+    { id: 17, category_id: 2, item_id: 2 },
+    { id: 18, category_id: 7, item_id: 7 },
+    { id: 19, category_id: 7, item_id: 8 }
+  ];
+
+});
--- app/service/jSQLService.js
+++ app/service/jSQLService.js
@@ -3,17 +3,27 @@ app.service('jSQL',function($document, $compile){
   const self = this;
 
   this.innerjoinArrays = function(arrOne, arrTwo, match_one, match_two, rename){
-    return arr = arrOne.filter(function(itemOne){
-      let matched = arrTwo.filter(function(itemTwo){
+    return arr = arrOne.filter(function(itemOne)
+    {
+      let matched = arrTwo.filter(function(itemTwo)
+      {
         return itemTwo[match_two] == itemOne[match_one];
       })[0]; // only first matched
-      if(matched){
+      if(matched)
+      {
         if(!rename){ self.copyUniqueProperties(itemOne, matched); }
+        else if(rename == 'overwrite'){ self.overrideProperties(itemOne, matched); }
+        else if(rename == 'keep_right'){ angular.copy(matched, itemOne); }
+        else if(rename == 'keep_left'){ angular.copy(itemOne, matched); }
         else{ self.attachNamedProperties(itemOne, matched, rename); }
       }
       return true;
     });
-  }
+  };
+
+  this.selectFromArray = function(name, equalsValue, inArray){
+    return inArray.filter(function(obj){ return obj[name] == equalsValue});
+  };
 
   this.copyUniqueProperties = function(target_obj, source_obj){
     for( let property in source_obj){
@@ -22,6 +32,17 @@ app.service('jSQL',function($document, $compile){
         if( target_property == property ){ exist = true; }
       }
       if(!exist){ target_obj[property] = source_obj[property]; }
+    }
+  }
+
+  this.overrideProperties = function(target_obj, source_obj){
+    for( let property in source_obj){
+      let exist = false;
+      for( let target_property in target_obj){
+        if( target_property == property ){ exist = true; }
+      }
+      if(exist){ target_obj[property] = source_obj[property]; }
+      else{ target_obj[property] = source_obj[property]; }
     }
   }
 
